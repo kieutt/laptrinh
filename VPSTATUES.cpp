0084b34096776db2f8a01a757837cb4d2b1e9467
Tống Thanh Kiều <46214275+kieutt@users.noreply.github.com>
2019-11-23 00:15:11 +0700

    Add files via upload

---
+++ VPSTATUES.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int M, cnt[300];
+char status;
+
+main()
+{
+    #ifndef KIEUQUOCDAT
+        freopen("vao.inp", "r", stdin);
+    #endif
+
+    scanf("%d\n", &M);
+    for (int i = 1; i <= M; i++)
+    {
+        scanf("%c", &status);
+        cnt[status]++;
+    }
+    printf("%d", M - max(cnt['E'], max(cnt['W'], max(cnt['S'], cnt['N']))));
+}
+
