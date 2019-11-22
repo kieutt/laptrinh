23d305e3311766f5ffc19db77df77762e77b4804
Tống Thanh Kiều <46214275+kieutt@users.noreply.github.com>
2019-11-22 23:58:42 +0700

    Add files via upload

---
+++ SWAPDG.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string N;
+int len, Nmin, Nmax;
+
+int get(string X)
+{
+    int hsgs = 0;
+    for (int i = 0; i < len; i++)
+        hsgs = hsgs * 10 + X[i] - '0';
+    return hsgs;
+}
+
+main()
+{
+    #ifndef KIEUQUOCDAT
+        freopen("vao.inp", "r", stdin);
+    #endif
+
+    cin >> N;   len = N.size();
+    Nmin = Nmax = get(N);
+    for (int i = 0; i < len; i++)
+        for (int j = i + 1; j < len; j++)
+        {
+            swap(N[i], N[j]);
+            if (N[0] != '0')
+            {
+                Nmin = min(Nmin, get(N));
+                Nmax = max(Nmax, get(N));
+            }
+            swap(N[i], N[j]);
+        }
+    cout << Nmin << " " << Nmax;
+}
